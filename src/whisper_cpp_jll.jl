3b69dbb40e1247848c60086bec9b5e0b9b3a4c23
jlbuild <juliabuildbot@gmail.com>
2026-09-23 16:08:25 +0000

    whisper_cpp_jll build 1.9.3+0

--- src/whisper_cpp_jll.jl
+++ src/whisper_cpp_jll.jl
@@ -2,8 +2,10 @@
 baremodule whisper_cpp_jll
 using Base
 using Base: UUID
+using LazyArtifacts
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("whisper_cpp")
-JLLWrappers.@generate_main_file("whisper_cpp", UUID("ba274cc4-b956-5b22-9ddf-0d83a922508d"))
+JLLWrappers.@generate_main_file("whisper_cpp", Base.UUID("ba274cc4-b956-5b22-9ddf-0d83a922508d"))
 end  # module whisper_cpp_jll
